9faa2e2466e5842427fe464e3a3339a6a05c9419
Alvin Li <aflip24@gmail.com>
2023-09-03 05:50:51 -0500

    finished lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.content
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body is not None:
+            try:
+                return json.loads(response_body)
+            except json.JSONDecodeError as e:
+                print(f"JSON Parsing Error: {e}")
+                return None
